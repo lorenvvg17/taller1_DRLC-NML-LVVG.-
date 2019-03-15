3c3fbf84420d097e2c56279713eb600fa074ee42
lorenvvg17 <48425678+lorenvvg17@users.noreply.github.com>
2019-03-15 09:54:40 -0500

    Add files via upload

---
+++ TALLER 1 FINAL-CORRECCIONES/scripts/nodo1.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+import rospy
+import serial, time
+import math
+#from std_msgs.msg import Integer
+from std_msgs.msg import String, Float64
+from geometry_msgs.msg import Twist
+
+class Node ():
+   	def __init__(self):
+        	self.rospy = rospy
+        	self.rospy.init_node("nodo_control1", anonymous = True)
+        	self.initParameters()
+        	self.initPublishers()
+        	self.main()
+
+    	def initParameters(self):        
+        	self.topic_lin = "/lineal"
+        	self.topic_ang = "/angular"
+		self.serial = "/serial"
+        	self.arduino = serial.Serial('/dev/ttyACM0', 9600)
+        	self.msg_lin = String()
+        	self.msg_ang = String()
+        	                     
+        	return
+   
+    	def initPublishers(self):
+       	 	self.pub_lin = self.rospy.Publisher(self.topic_lin, String, queue_size = 10)
+        	self.pub_ang = self.rospy.Publisher(self.topic_ang, String, queue_size = 10)
+        	return
+
+	
+    	def main (self):
+		#self.conversion=s(1023);
+        	print ("nodo OK")
+        	while not self.rospy.is_shutdown():
+			
+        		#self.rate.sleep()
+        		rawString = self.arduino.readline()
+             		self.msg_lin.data,self.msg_ang.data = rawString.split(",")
+             		self.msg_lin.data =self.msg_lin.data 
+			self.msg_ang.data = self.msg_ang.data 
+			self.pub_lin.publish(self.msg_lin)
+			self.pub_ang.publish(self.msg_ang)
+			print(self.msg_lin,self.msg_ang)
+
+if __name__=="__main__":
+    try:
+        print("Iniciando Nodo")
+        object = Node ()
+    except rospy.ROSInterruptException:
+        print ("Finalizando Nodo")
+        pass
---
+++ TALLER 1 FINAL-CORRECCIONES/scripts/nodo3.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+import rospy
+from std_msgs.msg import String
+from std_msgs.msg import Bool
+from geometry_msgs.msg import Twist
+
+
+class Node ():
+    def __init__(self):
+        self.rospy = rospy
+        self.rospy.init_node("nodo_control3", anonymous = True)
+        self.initParameters()
+        self.initSubscribers()
+        self.initPublishers()
+        self.main()
+
+    def initParameters(self):
+        	self.topic_lin = "/lineal"
+        	self.topic_ang = "/angular"
+        	self.topic_aux = "/aux_topic"
+        	self.topic_vel = "/cmd_vel"
+		self.V_lin = String()
+		self.V_ang = String()
+		self.msg_aux = Bool()
+    		self.msg_vel = Twist()
+        	self.change_V_lin = False
+        	self.change_V_ang = False
+        	self.change_aux = False
+        	self.rate = self.rospy.Rate(50)
+        	return
+
+    def callback_pot1(self, msg):
+        	self.V_lin = msg.data
+        	self.change_V_lin = True
+        	return
+
+    def callback_pot2(self, msg):
+        	self.V_ang = msg.data
+        	self.change_V_ang = True
+        	return
+
+    def callback_aux(self, msg):
+        	self.msg_aux = msg.data
+        	self.change_aux = True
+        	return
+
+    def initSubscribers(self):
+        
+        	self.sub_lin = self.rospy.Subscriber(self.topic_lin, String, self.callback_pot1)
+        	self.sub_ang = self.rospy.Subscriber(self.topic_ang, String, self.callback_pot2)
+        	self.sub_aux = self.rospy.Subscriber(self.topic_aux, Bool, self.callback_aux)
+       		return
+
+    def initPublishers(self):
+        
+       		self.pub_vel = self.rospy.Publisher(self.topic_vel, Twist, queue_size = 10)
+        	return
+
+
+    def main (self):
+        #CODIGO PRINCIPAL
+	print ("nodo OK")
+        
+        while not self.rospy.is_shutdown():
+            if self.change_V_ang and self.change_V_lin and self.change_aux:
+                    if self.msg_aux == True:
+                        self.msg_vel.linear.x= 0
+                        self.msg_vel.angular.z=float(self.V_ang)
+
+			#self.msg_vel=self.msg_vel*(0.0009)
+                        self.pub_vel.publish(self.msg_vel)
+			
+                    else:
+                        
+			self.msg_vel.linear.x = float(self.V_lin)
+                        self.msg_vel.angular.z = float(self.V_ang)
+			self.msg_vel.angular.z =(self.msg_vel.angular.z)/1023
+			self.msg_vel.linear.x =(self.msg_vel.linear.x)/1023
+			
+                        self.pub_vel.publish (self.msg_vel)
+			self.change_V_ang = False
+			self.change_V_lin = False
+			self.change_aux = False
+	
+        return
+	
+
+
+if __name__=="__main__":
+    try:
+        print("Iniciando Nodo")
+        object = Node ()
+    except rospy.ROSInterruptException:
+        print ("Finalizando Nodo")
+        pass
